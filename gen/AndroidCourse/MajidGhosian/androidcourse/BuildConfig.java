8960504be3046cbf470fc8e380f502ebdc37c986
dambo <majidghosian@gmail.com>
2014-10-04 00:21:31 +0330

    intial commit

---
+++ gen/AndroidCourse/MajidGhosian/androidcourse/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package AndroidCourse.MajidGhosian.androidcourse;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
